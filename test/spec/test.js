4ee8bb6d786096c7b4b34fd2d1f14011da2c8033
Tommy Barth <tommyhbrc@gmail.com>
2014-09-25 16:59:14 -0400

    initial commit, adds one enemy and attacks

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,31 @@
 /* global describe, it */
 
+
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
-            });
+    describe('Thug', function () {
+      beforeEach(function(){
+        this.thug = new Thug();
+        this.hero = new Hero();
+      });
+        it('should have an inital health of 200', function () {
+          expect(this.thug.getHealth()).to.equal(200);
+        });
+        it('should have the name Thug', function () {
+          expect(this.thug.getName()).to.equal('Thug');
+        });
+        it('should have a punch attack that deals 20 damage', function () {
+          this.thug.punchAttack(this.hero);
+          expect(this.hero.getHealth()).to.equal(180);
+        });
+        it('should have a shank attack that deals 50 damage', function () {
+          this.thug.shankAttack(this.hero);
+          expect(this.hero.getHealth()).to.equal(150);
+        });
+        it('should have a club attack that deals 70 damage', function () {
+          this.thug.clubAttack(this.hero);
+          expect(this.hero.getHealth()).to.equal(130);
         });
     });
 })();
