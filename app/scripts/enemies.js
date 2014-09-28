b298ea73defeb9b4831f08a1bc9d5eee9aac92d8
Allan Monzon <allanjmonzon@gmail.com>
2014-09-28 13:18:20 -0400

    close to complete game

--- app/scripts/enemies.js
+++ app/scripts/enemies.js
@@ -1,16 +1,219 @@
-function Hero() {
-  this.health = 200;
-}
+// function Hero() {
+//   this.health = 200;
+// }
 
-Hero.prototype.getHealth = function () {
-  return this.health ;
-};
+// Hero.prototype.getHealth = function () {
+//   return this.health ;
+// };
+
+// /////////THUG////////
+
+//     function Thug() {
+//       this.name = 'Thug';
+//       this.health = 400;
+//     }
+
+// Thug.prototype.getHealth = function () {
+//   return this.health ;
+// };
+
+// Thug.prototype.getName = function () {
+//   return this.name;
+// };
+
+// Thug.prototype.beatAttack = function (hero) {
+//   hero.health = (hero.health - (Math.floor(Math.random()*15) + 30));
+// };
+
+// Thug.prototype.clubAttack = function(hero) {
+//   hero.health = (hero.health - (Math.floor(Math.random()*25) + 50));
+// };
+
+// Thug.prototype.bludgeonAttack = function(hero) {
+//   hero.health = (hero.health - (Math.floor(Math.random()*35) + 70));
+// };
+
+
+// function beat() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 3) {
+//       console.log('Thug gave you a beatdown!');
+//       selectedEnemy.beatAttack(hero);
+//     } else {
+//       console.log("Thug missed");
+//     }
+// };
+
+// function club() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 4) {
+//       console.log('Thug hit you with a club');
+//       selectedEnemy.clubAttack(hero);
+//     } else {
+//       console.log("Thug missed");
+//     }
+// };
+
+// function bludgeon(hero) {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 5) {
+//       console.log('Thug gave you a bludgeoning!');
+//       selectedEnemy.bludgeonAttack(hero);
+//     } else {
+//       console.log("Thug missed");
+//     }
+// };
+
+
+
+// /////////Ruffian////////
+
+//     function Ruffian() {
+//       this.name = 'Ruffian';
+//       this.health = 300;
+//     }
+
+// Ruffian.prototype.getHealth = function () {
+// return this.health ;
+// };
+
+// Ruffian.prototype.getName = function () {
+// return this.name;
+// };
+
+// Ruffian.prototype.knifeAttack = function (hero) {
+// hero.health = (hero.health - (Math.floor(Math.random()*15) + 25));
+// };
+
+// Ruffian.prototype.pummelAttack = function(hero) {
+// hero.health = (hero.health - (Math.floor(Math.random()*20) + 40));
+// };
+
+// Ruffian.prototype.cudgelAttack = function(hero) {
+// hero.health = (hero.health - (Math.floor(Math.random()*25) + 60));
+// };
+
+
+// function knife() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 3) {
+//       console.log('Ruffian knifed you!');
+//       selectedEnemy.knifeAttack(hero);
+//     } else {
+//       console.log("Ruffian missed");
+//     }
+// };
+
+// function pummel() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 4) {
+//       console.log('Ruffian pummeled you!');
+//       selectedEnemy.pummelAttack(hero);
+//     } else {
+//       console.log("Ruffian missed");
+//     }
+// };
+
+// function cudgel() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 5) {
+//       console.log('Ruffian cudgeled you!');
+//       selectedEnemy.cudgelAttack(hero);
+//     } else {
+//       console.log("Ruffian missed");
+//     }
+// };
+
+// function randomRuffianAttack(){
+//   var attack = Math.floor(Math.random()*10);
+//   if (attack <=5) {
+//     knife(selectedHero);
+//   } else if (8.5>= attack && attack > 5) {
+//     pummel(selectedHero);
+//   } else {
+//     cudgel(selectedHero);
+//   }
+
+// };
+
+
+
+// /////////Hoodlum////////
+
+// function Hoodlum() {
+//   this.name = 'Hoodlum';
+//   this.health = 200;
+// }
+
+// Hoodlum.prototype.getHealth = function () {
+// return this.health ;
+// };
+
+// Hoodlum.prototype.getName = function () {
+// return this.name;
+// };
+
+// Hoodlum.prototype.pocketKnifeAttack = function (hero) {
+// hero.health = (hero.health - (Math.floor(Math.random()*10) + 15));
+// };
+
+// Hoodlum.prototype.batonAttack = function(hero) {
+// hero.health = (hero.health - (Math.floor(Math.random()*20) + 25));
+// };
+
+// Hoodlum.prototype.shankAttack = function(hero) {
+// hero.health = (hero.health - (Math.floor(Math.random()*30) + 70));
+// };
+
+// function pocketKnife() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 3) {
+//       console.log('Hoodlum pocketknifed you!');
+//       selectedEnemy.pocketKnifeAttack(hero);
+//     } else {
+//       console.log("Hoodlum missed");
+//     }
+// };
+
+// function baton() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 4) {
+//       console.log('Hoodlum hit you with a baton!');
+//       selectedEnemy.batonAttack(hero);
+//     } else {
+//       console.log("Hoodlum missed");
+//     }
+// };
+
+// function shank() {
+//   hit = Math.floor(Math.random()*10);
+//   if (hit > 5) {
+//       console.log('Hoodlum shanked you!');
+//       selectedEnemy.shankAttack(hero);
+//     } else {
+//       console.log("Hoodlum missed");
+//     }
+// };
+
+// function randomHoodlumAttack(){
+//   var attack = Math.floor(Math.random()*10);
+//   if (attack <=5) {
+//     pocketKnife(selectedHero);
+//   } else if (8.5>= attack && attack > 5) {
+//     baton(selectedHero);
+//   } else {
+//     shank(selectedHero);
+//   }
+
+// };
+
+
 
 /////////THUG////////
 
     function Thug() {
       this.name = 'Thug';
-      this.health = 400;
+      this.health = 550;
     }
 
 Thug.prototype.getHealth = function () {
@@ -21,26 +224,55 @@ Thug.prototype.getName = function () {
   return this.name;
 };
 
-Thug.prototype.punchAttack = function (hero) {
-  hero.health = (hero.health - (Math.floor(Math.random()*15) + 30));
+Thug.prototype.beatAttack = function () {
+  selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*15) + 30));
 };
 
-Thug.prototype.clubAttack = function(hero) {
-  hero.health = (hero.health - (Math.floor(Math.random()*25) + 50));
+Thug.prototype.clubAttack = function() {
+  selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*25) + 50));
 };
 
-Thug.prototype.bludgeonAttack = function(hero) {
-  hero.health = (hero.health - (Math.floor(Math.random()*35) + 70));
+Thug.prototype.bludgeonAttack = function() {
+  selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*35) + 70));
 };
 
 var thug = new Thug()
 
+function beat() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 3) {
+      console.log('Thug gave you a beatdown!');
+      selectedEnemy.beatAttack();
+    } else {
+      console.log("Thug missed");
+    }
+};
+
+function club() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 4) {
+      console.log('Thug hit you with a club');
+      selectedEnemy.clubAttack();
+    } else {
+      console.log("Thug missed");
+    }
+};
+
+function bludgeon(hero) {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 5) {
+      console.log('Thug gave you a bludgeoning!');
+      selectedEnemy.bludgeonAttack();
+    } else {
+      console.log("Thug missed");
+    }
+};
 
 /////////Ruffian////////
 
     function Ruffian() {
       this.name = 'Ruffian';
-      this.health = 300;
+      this.health = 450;
     }
 
 Ruffian.prototype.getHealth = function () {
@@ -51,23 +283,65 @@ Ruffian.prototype.getName = function () {
 return this.name;
 };
 
-Ruffian.prototype.knifeAttack = function (hero) {
-hero.health = (hero.health - (Math.floor(Math.random()*15) + 25));
+Ruffian.prototype.knifeAttack = function () {
+selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*15) + 25));
 };
 
-Ruffian.prototype.pummelAttack = function(hero) {
-hero.health = (hero.health - (Math.floor(Math.random()*20) + 40));
+Ruffian.prototype.pummelAttack = function() {
+selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*20) + 40));
 };
 
-Ruffian.prototype.cudgelAttack = function(hero) {
-hero.health = (hero.health - (Math.floor(Math.random()*25) + 60));
+Ruffian.prototype.cudgelAttack = function() {
+selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*25) + 60));
 };
+
+function knife() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 3) {
+      console.log('Ruffian knifed you!');
+      selectedEnemy.knifeAttack();
+    } else {
+      console.log("Ruffian missed");
+    }
+};
+
+function pummel() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 4) {
+      console.log('Ruffian pummeled you!');
+      selectedEnemy.pummelAttack();
+    } else {
+      console.log("Ruffian missed");
+    }
+};
+
+function cudgel() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 5) {
+      console.log('Ruffian cudgeled you!');
+      selectedEnemy.cudgelAttack();
+    } else {
+      console.log("Ruffian missed");
+    }
+};
+
+function randomRuffianAttack(){
+  var attack = Math.floor(Math.random()*10);
+  if (attack <=5) {
+    knife(selectedHero);
+  } else if (8.5>= attack && attack > 5) {
+    pummel(selectedHero);
+  } else {
+    cudgel(selectedHero);
+  }
+
+}
 
 /////////Hoodlum////////
 
 function Hoodlum() {
   this.name = 'Hoodlum';
-  this.health = 200;
+  this.health = 350;
 }
 
 Hoodlum.prototype.getHealth = function () {
@@ -78,18 +352,56 @@ Hoodlum.prototype.getName = function () {
 return this.name;
 };
 
-Hoodlum.prototype.pocketKnifeAttack = function (hero) {
-hero.health = (hero.health - (Math.floor(Math.random()*10) + 15));
+Hoodlum.prototype.pocketKnifeAttack = function () {
+selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*10) + 15));
 };
 
-Hoodlum.prototype.batonAttack = function(hero) {
-hero.health = (hero.health - (Math.floor(Math.random()*20) + 25));
+Hoodlum.prototype.batonAttack = function() {
+selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*20) + 25));
 };
 
-Hoodlum.prototype.shankAttack = function(hero) {
-hero.health = (hero.health - (Math.floor(Math.random()*30) + 70));
+Hoodlum.prototype.shankAttack = function() {
+selectedHero.health = (selectedHero.health - (Math.floor(Math.random()*30) + 70));
 };
 
+function pocketKnife() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 3) {
+      console.log('Hoodlum pocketknifed you!');
+      selectedEnemy.pocketKnifeAttack();
+    } else {
+      console.log("Hoodlum missed");
+    }
+};
 
+function baton() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 4) {
+      console.log('Hoodlum hit you with a baton!');
+      selectedEnemy.batonAttack();
+    } else {
+      console.log("Hoodlum missed");
+    }
+};
 
+function shank() {
+  hit = Math.floor(Math.random()*10);
+  if (hit > 5) {
+      console.log('Hoodlum shanked you!');
+      selectedEnemy.shankAttack();
+    } else {
+      console.log("Hoodlum missed");
+    }
+};
 
+function randomHoodlumAttack(){
+  var attack = Math.floor(Math.random()*10);
+  if (attack <=5) {
+    pocketKnife(selectedHero);
+  } else if (8.5>= attack && attack > 5) {
+    baton(selectedHero);
+  } else {
+    shank(selectedHero);
+  }
+
+}
---
+++ app/scripts/main.js
@@ -0,0 +1,567 @@
+var thug,
+    hoodlum,
+    ruffian, 
+    batman,
+    yeti,
+    jackaloupe,
+    hit,
+    enemy1, 
+    enemy2, 
+    enemy3, 
+    selectedEnemy,
+    selectedHero;
+
+
+$(function() {
+  // thug = new Thug;
+  // hoodlum = new Hoodlum;
+  // ruffian = new Ruffian;
+  // batman = new Batman;
+  // jackaloupe = new Jackaloupe;
+  // yeti = new Yeti;
+});
+
+
+
+// Choose Your Hero //
+
+
+$(".hero-button").on("click", function(event) {
+  event.preventDefault();
+  var selected = $("#hero-drop option:selected");
+
+  $("#hero-con").hide();
+
+  $("#vs").removeClass("hidden");
+  $("#vs").addClass("vs");
+
+  if (selected.val() == "Batman") {
+
+    selectedHero = new Batman();
+    $("#batman").removeClass("hidden");
+    $("#batman").addClass("batman");
+    $("body").addClass("bat-back");
+  }
+
+  if (selected.val() == "Yeti") {
+
+    selectedHero = new Yeti();
+    $("#yeti").removeClass("hidden");
+    $("#yeti").addClass("yeti");
+    $("body").addClass("yeti-back");
+  }
+
+  if (selected.val() == "Jackaloupe") {
+
+    selectedHero = new Jackaloupe();
+    $("#jackaloupe").removeClass("hidden");
+    $("#jackaloupe").addClass("jackaloupe");
+
+  }
+
+});
+
+
+// Choose Random Enemies //
+
+
+$(".generate").on("click", function() {
+  event.preventDefault();
+
+  $("#enemy-con").hide();
+
+  var hit1 = Math.floor(Math.random()*10);
+  if (hit1 <=5) {
+
+    enemy1 = new Hoodlum();
+    $("#enemy-one").removeClass("hidden");
+    $("#enemy-one").addClass("hoodlum");
+
+
+  } else if (8.5>= hit1 && hit1 > 5) {
+
+    enemy1 = new Ruffian();
+    $("#enemy-one").removeClass("hidden");
+    $("#enemy-one").addClass("ruffian");
+
+  } else {
+
+    enemy1 = new Thug();
+    $("#enemy-one").removeClass("hidden");
+    $("#enemy-one").addClass("thug");
+
+  }
+
+  var hit2 = Math.floor(Math.random()*10);
+  if (hit2 <=5) {
+
+    enemy2 = new Hoodlum();
+    $("#enemy-two").removeClass("hidden");
+    $("#enemy-two").addClass("hoodlum");
+
+  } else if (8.5>= hit2 && hit2 > 5) {
+
+    enemy2 = new Ruffian();
+    $("#enemy-two").removeClass("hidden");
+    $("#enemy-two").addClass("ruffian");
+
+  } else {
+
+    enemy2 = new Thug();
+    $("#enemy-two").removeClass("hidden");
+    $("#enemy-two").addClass("thug");
+
+  }
+
+  var hit3 = Math.floor(Math.random()*10);
+  if (hit3 <=5) {
+
+    enemy3 = new Hoodlum();
+    $("#enemy-three").removeClass("hidden");
+    $("#enemy-three").addClass("hoodlum");
+
+  } else if (8.5>= hit3 && hit3 > 5) {
+
+    enemy3 = new Ruffian();
+    $("#enemy-three").removeClass("hidden");
+    $("#enemy-three").addClass("ruffian");
+
+  } else {
+
+    enemy3 = new Thug();
+    $("#enemy-three").removeClass("hidden");
+    $("#enemy-three").addClass("thug");
+
+  }
+
+});
+
+
+// Selecting Enemies //
+
+$("#enemy-one").on('click', function() {
+  selectedEnemy = enemy1;
+  console.log(selectedEnemy);
+
+  $(this).toggleClass("enemy-selected")
+  .siblings().removeClass("enemy-selected");
+});
+
+$("#enemy-two").on('click', function() {
+  selectedEnemy = enemy2;
+  console.log(selectedEnemy);
+
+  $(this).toggleClass("enemy-selected")
+  .siblings().removeClass("enemy-selected");
+});
+
+$("#enemy-three").on('click', function() {
+  selectedEnemy = enemy3;
+  console.log(selectedEnemy);
+
+  $(this).toggleClass("enemy-selected")
+  .siblings().removeClass("enemy-selected");
+});
+
+
+////Enemies random attack functions////
+
+function randomThugAttack(hero){
+  var attack = Math.floor(Math.random()*10);
+  if (attack <=5) {
+    beat(hero);
+  } else if (8.5>= attack && attack > 5) {
+    club(hero);
+  } else {
+    bludgeon(hero);
+  }
+}
+
+function randomRuffianAttack(hero){
+  var attack = Math.floor(Math.random()*10);
+  if (attack <=5) {
+    knife(hero);
+  } else if (8.5>= attack && attack > 5) {
+    pummel(hero);
+  } else {
+    cudgel(hero);
+  }
+}
+
+function randomHoodlumAttack(hero){
+  var attack = Math.floor(Math.random()*10);
+  if (attack <=5) {
+    pocketKnife(hero);
+  } else if (8.5>= attack && attack > 5) {
+    baton(hero);
+  } else {
+    shank(hero);
+  }
+}
+
+function enemy1RandomRetaliation() {
+  if (enemy1 instanceof Thug) {
+    randomThugAttack();
+  } else if (enemy1 instanceof Ruffian){
+    randomRuffianAttack();
+  } else {
+    randomHoodlumAttack();
+  }
+}
+
+function enemy2RandomRetaliation() {
+  if (enemy2 instanceof Thug) {
+    randomThugAttack();
+  } else if (enemy2 instanceof Ruffian){
+    randomRuffianAttack();
+  } else {
+    randomHoodlumAttack();
+  }
+}
+
+function enemy3RandomRetaliation() {
+  if (enemy3 instanceof Thug) {
+    randomThugAttack();
+  } else if (enemy3 instanceof Ruffian){
+    randomRuffianAttack();
+  } else {
+    randomHoodlumAttack();
+  }
+}
+
+/// Enemies Health Bar ///
+
+function enemyHealth() {
+  if (selectedEnemy === enemy3) {
+       (document.getElementById('enemy3-health').value = selectedEnemy.health);
+  } else if (selectedEnemy === enemy2) {
+      (document.getElementById('enemy2-health').value = selectedEnemy.health);
+  } else if (selectedEnemy === enemy1){
+      (document.getElementById('enemy1-health').value = selectedEnemy.health);
+  }
+}
+
+
+/// Batman ///
+
+function batHealth() {
+  var health = document.getElementById('bat-health').value = selectedHero.health;
+  if (health <= 0 ) {
+    $('.right').append("<h2>You Lose<h2>");
+  }
+}
+
+$('.batarang').on('click', function() { 
+  batarang(selectedEnemy);
+  enemyHealth()
+ 
+ // enemy2Health()
+ // enemy3Health()
+  // console.log(batman.getHealth());
+ 
+  // console.log(thug.getHealth());
+  // console.log(stop(thug));
+  //  console.log(selectedEnemy.name + ': ' +  selectedEnemy.getHealth());
+  //   console.log('Batman: ' + batman.getHealth());  
+  // console.log(selectedEnemy.name + ': ' +  selectedEnemy.getHealth());
+
+  setTimeout(function() {
+    batHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+    // console.log('Thug attacks back');
+    // console.log('Batman: ' + batman.getHealth());
+    // console.log('Thug: ' + thug.getHealth());
+    // club(batman);
+    // console.log('Batman: ' + batman.getHealth());
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+
+$('.punch').on('click', function() { 
+  punch(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    batHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+$('.kick').on('click', function() { 
+  kick(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    batHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+
+/// Yeti ///
+
+function yetiHealth() {
+  return document.getElementById('yeti-health').value = selectedHero.health;
+}
+
+$('.bash').on('click', function() { 
+  bash(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    yetiHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+$('.smash').on('click', function() { 
+  smash(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    yetiHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+$('.whallop').on('click', function() { 
+  whallop(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    yetiHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+
+
+
+/// Jackaloupe ///
+
+function jackHealth() {
+  return document.getElementById('jack-health').value = selectedHero.health;
+}
+
+$('.stab').on('click', function() { 
+  stab(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    jackHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+$('.maul').on('click', function() { 
+  maul(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    jackHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+$('.flyingRabbitKick').on('click', function() { 
+  flyingRabbitKick(selectedEnemy);
+  enemyHealth()
+
+  setTimeout(function() {
+    jackHealth();
+    selectedEnemy = enemy1;
+      enemy1RandomRetaliation();
+
+      selectedEnemy = enemy2;
+      enemy2RandomRetaliation();
+
+      selectedEnemy = enemy3;
+      enemy3RandomRetaliation();
+  
+  }, 500);
+   if (selectedEnemy === enemy1) {
+
+      $("#enemy-one").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy2) {
+
+      $("#enemy-two").removeClass("enemy-selected");
+
+    } else if (selectedEnemy === enemy3) {
+
+      $("#enemy-three").removeClass("enemy-selected");
+
+    }
+});
+
+
+
+
+
